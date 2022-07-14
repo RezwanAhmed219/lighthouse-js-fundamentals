5017aeff1943f8a2929a261d6cc9531e9fdf39fc
RezwanAhmed219 <rezwan.ahmed.258@gmail.com>
2022-07-13 23:14:57 -0400

    update

---
+++ lighthouse-js-fundamentals/Rooting-for-you.js
@@ -0,0 +1,20 @@
+const judgeVegetable = function (vegetables, metric) {
+  // Your code in here ...
+
+  let measure = metric;
+  if (measure === metric) {
+    let scores = [];
+    for (let i = 0; i < vegetables.length; i++) {
+      scores.push(vegetables[i][metric]);
+    }
+    let winner = Math.max(...scores);
+    for (let y = 0; y < vegetables.length; y++) {
+      if (winner === vegetables[y][metric]) {
+        let winnerName = vegetables[y].submitter;
+        return winnerName;
+      }
+    }
+  } else {
+    return undefined;
+  }
+};
